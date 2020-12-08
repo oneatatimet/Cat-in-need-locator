30fd200b2fe79f7efbf395615c0d457375bad465
Nada <olayan.nada@nagoya-u.jp>
2020-12-08 17:30:22 +0100

    basic user mutations decided

--- Graphql/resolvers/users/resolvers/mutation/index.js
+++ Graphql/resolvers/users/resolvers/mutation/index.js
@@ -1,7 +1,15 @@
 const createUser = require('./createUser.mutation');
+const login = require('./login.mutation');
+const deleteUserById = require('./deleteUserById.mutation');
+const updateUser = require('./updateUser.mutation');
+const changePassword = require('./changePassword.mutation');
 
 let Mutation = {
 	createUser,
+	login,
+	deleteUserById,
+	updateUser,
+	changePassword,
 };
 
 module.exports = Mutation;
