8d07251afc5675860327aa25933ed122c33c2cb2
Nada <olayan.nada@nagoya-u.jp>
2020-12-08 17:20:27 +0100

    create user works

--- Graphql/resolvers/users/resolvers/mutation/createUser.mutation.js
+++ Graphql/resolvers/users/resolvers/mutation/createUser.mutation.js
@@ -1,6 +1,13 @@
 const userService = require('../../../../../services/user.services');
 
 const createUser = async (parent, args) => {
+	let conn = args.connections;
+	let NoOfConRequestInSingleAPICall = 5;
+	let daysInCurrentMonth = new Date(new Date().getMonth(), new Date().getFullYear(), 0).getDate();
+	let perDayConn = Math.ceil(conn / daysInCurrentMonth);
+	let hours = Math.ceil(perDayConn / NoOfConRequestInSingleAPICall);
+	args.hoursRange = `${hours}`;
+
 	let newUser = await userService.createUser(args);
 	return newUser;
 };
