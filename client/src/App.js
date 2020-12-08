51f528c29f7a3f3ec25c6f3ecaec964babe4cbb8
Nada <olayan.nada@nagoya-u.jp>
2020-12-08 23:30:41 +0100

    created login page style only, need to return token after submit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 
-//import Login from './pages/Login';
+//import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 const App = (props) => {
 	return (
 		<BrowserRouter>
 			<Switch>
+				<Route path="/" exact component={Login} />
+				<Route path="/dashboard" />
+				<Route path="/login" exact component={Login} />
 				<Route path="/signup" exact component={Signup} />
 			</Switch>
 		</BrowserRouter>
