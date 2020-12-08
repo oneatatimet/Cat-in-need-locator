0791d0efa3db9ec174e8cb0aa1048c2725a88287
Nada <olayan.nada@nagoya-u.jp>
2020-12-08 15:30:40 +0100

    user model created and database is synced

---
+++ models/user.js
@@ -0,0 +1,34 @@
+//created the model using cli command
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+	class User extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	User.init(
+		{
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			email: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			passwrod: { type: DataTypes.STRING, allowNull: false },
+			token: DataTypes.VIRTUAL,
+		},
+		{
+			sequelize,
+			modelName: 'User',
+		}
+	);
+	return User;
+};
