b925b47fe0eb9006baba6677691774cb9cd5498c
Nada <olayan.nada@nagoya-u.jp>
2020-12-09 02:51:31 +0100

    corrected logout and added header

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Header from '../../components/Header';
+import Box from '@material-ui/core/Box';
+
+function Copyright() {
+	return (
+		<Typography variant="body2" color="textSecondary" align="center">
+			{'Copyright © '}
+			<Link color="inherit" href="https://material-ui.com/">
+				Your Website
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
+}
 
 export default function Dashboard(props) {
-	function Copyright() {
-		return (
-			<Typography variant="body2" color="textSecondary" align="center">
-				{'Copyright © '}
-				<Link color="inherit" href="https://material-ui.com/">
-					Your Website
-				</Link>{' '}
-				{new Date().getFullYear()}
-				{'.'}
-			</Typography>
-		);
-	}
-
 	return (
 		<div>
-			<h1>Dashboard</h1>
+			<Header />
 			<Box mt={8}>
 				<Copyright />
 			</Box>
